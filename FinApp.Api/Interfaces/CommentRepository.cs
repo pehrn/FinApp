c52c42ed50d68a080239f4e8a25d161881cb1afe
Peter <pogo6348@pm.me>
2025-03-28 08:34:53 +0100

    Restructured solution, moved tests

--- FinApp.Api/Interfaces/CommentRepository.cs
+++
@@ -1,9 +0,0 @@
-using FinApp.Api.Models;
-
-namespace FinApp.Api.Interfaces;
-
-public interface ICommentRepository
-{
-    Task<List<Comment>> GetAllAsync();
-    Task<Comment?> GetByIdAsync(int id);
-}
