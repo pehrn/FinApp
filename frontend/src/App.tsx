18014049ff6c96937d6b72f7b0dd1d79b92fd121
Peter <pogo6348@pm.me>
2025-04-17 10:18:13 +0200

    Handle search events

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,35 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {ChangeEvent, SyntheticEvent, useState } from 'react';
+import { searchCompanies } from './api';
 import './App.css';
+import { CompanySearch } from './company';
 import CardList from './Components/CardList/CardList';
 import Search from './Components/Search/Search';
 
 function App() {
+    const [search, setSearch] = useState<string>("");
+    const [searchResults, setSearchResults] = useState<CompanySearch[]>([]);
+    const [serverError, setServerError] = useState<string>("");
+    
+    const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+        setSearch(e.target.value);
+        // console.log(e);
+    };
+    
+    const onClick = async (e: SyntheticEvent) => {
+        const result = await searchCompanies(search);
+        
+        if (typeof result === "string") {
+            setServerError(result);
+        } else if (Array.isArray(result.data)) {
+            setSearchResults(result.data);
+        }
+        
+        // console.log(searchResults);
+    };
+    
   return (
     <div className="App">
-        <Search />
+        <Search onClick={onClick} search={search} handleChange={handleChange}/>
         <CardList />
     </div>
   );
--- frontend/src/Components/Search/Search.tsx
+++ frontend/src/Components/Search/Search.tsx
@@ -1,24 +1,18 @@
-import React, { ChangeEvent, JSX, useState, MouseEvent, SyntheticEvent } from 'react'
+import React, { ChangeEvent, JSX, SyntheticEvent } from 'react'
 
-interface Props {}
+interface Props {
+    onClick: (e: SyntheticEvent) => void;
+    search: string | undefined;
+    handleChange: (e: ChangeEvent<HTMLInputElement>) => void;
+};
 
-const Search : React.FC<{}> = (props: Props): JSX.Element => {
-    const [search, setSearch] = useState<string>("");
-    
-    const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
-        setSearch(e.target.value);
-    };
-    
-    const onClick = (e: SyntheticEvent) => {
-        console.log(e);
-    };
-    
+const Search: React.FC<Props> = ({ onClick, search, handleChange }: Props): JSX.Element => {
     return (
         <div>
             <input value={search} onChange={(e) => handleChange(e)}></input>
-            <button onClick={(e) => onClick(e)}></button>
+            <button onClick={(e) => onClick(e)}>Search</button>
         </div>
     )
 }
 
-export default Search
+export default Search; 
