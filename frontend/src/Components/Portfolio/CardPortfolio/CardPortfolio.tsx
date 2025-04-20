dc003564cf7567b81a0145e838f14b756aac3e36
Peter <pogo6348@pm.me>
2025-04-20 08:32:58 +0200

    Added react router; Split app into pages

--- frontend/src/Components/Portfolio/CardPortfolio/CardPortfolio.tsx
+++ frontend/src/Components/Portfolio/CardPortfolio/CardPortfolio.tsx
@@ -1,4 +1,5 @@
 import React, {SyntheticEvent} from 'react';
+import { Link } from 'react-router-dom';
 import DeleteFromPortfolio from '../DeleteFromPortfolio/DeleteFromPortfolio';
 
 interface Props {
@@ -9,7 +10,7 @@ interface Props {
 const CardPortfolio = ({ portfolioValue, onDeleteFromPortfolio }: Props) => {
     return (
         <div className="flex flex-col w-full p-8 space-y-4 text-center rounded-lg shadow-lg md:w-1/3">
-            <p className="pt-6 text-xl font-bold">{portfolioValue}</p>
+            <Link to={`/company/${portfolioValue}`} className="pt-6 text-xl font-bold">{portfolioValue}</Link>
             <DeleteFromPortfolio
                 portfolioValue={portfolioValue}
                 onDeleteFromPortfolio={onDeleteFromPortfolio}
