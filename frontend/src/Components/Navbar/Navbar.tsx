8ad33a9f0819c5403c7db6020b12645203e0d55f
Peter <pogo6348@pm.me>
2025-04-21 09:13:29 +0200

    Conditional loggedIn in navbar

--- frontend/src/Components/Navbar/Navbar.tsx
+++ frontend/src/Components/Navbar/Navbar.tsx
@@ -1,12 +1,16 @@
 import React from "react";
 import { FaMagnifyingGlass } from "react-icons/fa6";
 import { Link } from "react-router-dom";
+import { useAuth } from "../../Context/useAuth";
 import logo from "./logo2.png";
 import "./Navbar.css";
 
 interface Props {}
 
 const Navbar = (props: Props) => {
+    
+    const { isLoggedIn, user, logOut } = useAuth();
+    
     return (
         <nav className="relative container mx-auto p-6">
             <div className="flex items-center justify-between">
@@ -21,15 +25,25 @@ const Navbar = (props: Props) => {
                         </Link>
                     </div>
                 </div>
-                <div className="hidden lg:flex items-center space-x-6 text-back">
-                    <Link to="login" className="hover:text-darkBlue">Login</Link>
-                    <Link
-                        to="signup"
-                        className="px-8 py-3 font-bold rounded text-white bg-lightGreen hover:opacity-70"
-                    >
-                        Signup
-                    </Link>
-                </div>
+                { isLoggedIn() ? (
+                    <div className="hidden lg:flex items-center space-x-6 text-back">
+                        <div className="hover:text-darkBlue">Welcome back, { user?.userName }.</div>
+                        <a onClick={logOut} className="px-4 py-1 font-bold rounded text-white bg-red-500 hover:opacity-70">
+                            Logout
+                        </a>
+                    </div>
+                ) : (
+                    <div className="hidden lg:flex items-center space-x-6 text-back">
+                        <Link to="/login" className="hover:text-darkBlue">Login</Link>
+                        <Link
+                            to="/signup"
+                            className="px-8 py-3 font-bold rounded text-white bg-lightGreen hover:opacity-70"
+                        >
+                            Signup
+                        </Link>
+                    </div>
+                ) }
+
             </div>
         </nav>
     );
