9a89c41ef38616eb039c3cd2505f850d005a3ec1
Peter <pogo6348@pm.me>
2025-04-21 07:37:22 +0200

    Implemented authentication services

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,14 @@
 import { Outlet } from 'react-router-dom';
+import { ToastContainer } from 'react-toastify';
+import "react-toastify/dist/ReactToastify.css";
 import './App.css';
 import Navbar from './Components/Navbar/Navbar';
 
 function App() {
   return <>
     <Navbar />
-      <Outlet />
+    <Outlet />
+    <ToastContainer />
   </>;
 }
 
